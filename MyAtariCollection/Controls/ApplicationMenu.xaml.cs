411410a3cb7c030706e137d144451859720efd63
David Black <david_black_uk@yahoo.com>
2024-01-13 13:20:20 +0000

    Added new global menu bar and removed buttons from systen selector and system editor.
    I also removed the Menubar code as it doesn't work on Windows

---
+++ MyAtariCollection/Controls/ApplicationMenu.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAtariCollection.Controls;
+
+public partial class ApplicationMenu : ContentView
+{
+    public ApplicationMenu()
+    {
+        InitializeComponent();
+    }
+}
